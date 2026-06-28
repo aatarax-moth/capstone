8c4922ccd6a72231b06faa640f1c5ffb19fb98ae
Kinesha Kay <kineshakay1@gmail.com>
2026-06-28 16:33:15 +1000

    fixed

--- src/components/bookingpage.js
+++ src/components/bookingpage.js
@@ -1,26 +1,24 @@
-import './bookingpage.css';
-import BookingForm from './bookingform.js';
-import BookingHero from './bookinghero.js';
+import "./bookingpage.css";
+import BookingForm from "./bookingform.js";
+import BookingHero from "./bookinghero.js";
 
-function BookingPage({availableTimes, dispatch, setAvailableTimes}) {
-
-    async function submitForm(formData) {
-        try {
-            const response = await submitAPI(formData);
-            console.log("Submitted Data Successful:", response);
-            return response;
-        } catch (error) {
-            console.error("Error submitting form:", error);
-            throw error;
-        }
-    }
-    
-    return (
-        <div className="bookingpage">
-            <BookingHero/>
-            <BookingForm availableTimes={availableTimes} dispatch={dispatch} setAvailableTimes={setAvailableTimes} onSubmit={submitForm}/>
-        </div>
-    );
+function BookingPage({
+  availableTimes,
+  dispatch,
+  setAvailableTimes,
+  submitForm,
+}) {
+  return (
+    <div className="bookingpage">
+      <BookingHero />
+      <BookingForm
+        availableTimes={availableTimes}
+        dispatch={dispatch}
+        setAvailableTimes={setAvailableTimes}
+        onSubmit={submitForm}
+      />
+    </div>
+  );
 }
 
 export default BookingPage;
--- src/components/hero.js
+++ src/components/hero.js
@@ -13,7 +13,7 @@ function Hero() {
         <h1>Little Lemon</h1>
         <p>Welcome to Little Lemon, your new favorite neighborhood bistro! We serve simple, delicious food made with the freshest ingredients, sourced locally whenever possible. Our menu features a variety of dishes designed to cater to every palate, from comforting classics to innovative seasonal specials. Whether you’re stopping by for a quick lunch, a cozy dinner, or a relaxed weekend brunch, our warm and inviting atmosphere is the perfect backdrop for enjoying great food with friends and family. Come and experience the taste of Little Lemon, where every meal is crafted with love and care!</p>
         <Link to="/booking">
-        <Button text="Reserve a Table"/>
+        <button aria-label="On Click"  text="Reserve a Table"/>
         </Link>
         </div>
       </div>
